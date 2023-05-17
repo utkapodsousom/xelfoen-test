e978feea968305346652b0b3f47f1e9e7d842448
utkapodsousom <utkapodsousom@gmail.com>
2023-05-17 09:49:25 +0300

    fix base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/xelfoen-test/',
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/xelfoen-test/'
+  }
+
+  return config
+});
